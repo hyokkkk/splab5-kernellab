cea2fb298fc34b01681086c92bf722486626135a
hyokkkk <tahyok7123@naver.com>
2021-05-27 05:52:32 +0900

    Splab5: Part1 fin - using arr

--- ptree/dbfs_ptree.c
+++ ptree/dbfs_ptree.c
@@ -2,59 +2,105 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
+#define MAXLEN 1000
 
-static struct dentry *dir, *inputdir, *ptreedir;
+static struct dentry *dir, *inputfile, *ptreefile;
 static struct task_struct *curr;
+ssize_t total_len= 0;
+char output[MAXLEN];
 
+// todo 1 : change to kmalloc
+// todo 2 : use whole name instead of comm
+
+// called when cmd gets `echo 1234 >> input`.
 static ssize_t write_pid_to_input(struct file *fp, 
-                                const char __user *user_buffer, 
-                                size_t length, 
-                                loff_t *position)
+                            const char __user *user_buffer, 
+                            size_t length, 
+                            loff_t *position)
 {
-        pid_t input_pid;
+  pid_t input_pid;
 
-        //sscanf(user_buffer, "%u", &input_pid);
-        //curr = // Find task_struct using input_pid. Hint: pid_task
+  memset(output, 0, MAXLEN);
+  char temp_output[MAXLEN];
+  ssize_t temp_len = 0;
 
-        // Tracing process tree from input_pid to init(1) process
+  // Get input
+  sscanf(user_buffer, "%u", &input_pid);
 
-        // Make Output Format string: process_command (process_id)
+  // Find task_struct using input_pid. Hint: pid_task
+  if (!(curr = pid_task(find_vpid(input_pid), PIDTYPE_PID))){ 
+    printk("Invalid pid\n");
+    return -1; 
+  }
 
-        return length;
+
+  // Tracing process tree from input_pid to init(1) process
+  while (curr->pid !=0){
+    // init to 0
+    memset(temp_output, 0, MAXLEN);
+    // comm : 15chars
+    temp_len= snprintf(temp_output, MAXLEN, "%s (%d)\n", curr->comm, curr->pid);
+    //append the last result 
+    snprintf(temp_output+temp_len, MAXLEN-temp_len, output);
+    //add to the total length
+    if(temp_len>= 0) { total_len+= temp_len; }
+    //update output string
+    strcpy(output, temp_output);
+    //go to parent
+    curr = curr -> parent;
+
+  }
+
+  // Make Output Format string: process_command (process_id)
+
+  return length;
 }
 
-static const struct file_operations dbfs_fops = {
-        .write = write_pid_to_input,
+static ssize_t read_output_from_ptreefile(struct file *fp, 
+                                          char __user *user_buffer, 
+                                          size_t length, 
+                                          loff_t *position)
+{
+  // read the output from kernel buffer to user buffer
+  return simple_read_from_buffer(user_buffer, length, position, output, total_len);
+}
+
+static const struct file_operations dbfs_fops_write = {
+  .write = write_pid_to_input,
+};
+
+static const struct file_operations dbfs_fops_read = {
+  .read = read_output_from_ptreefile,
 };
 
 static int __init dbfs_module_init(void)
 {
-        // Implement init module code
+  if (!(dir = debugfs_create_dir("ptree", NULL))) {
+    printk("Cannot create ptree dir\n");
+    return -1;
+  }
+  // 00700: allow read, write, execute mode to owner.
+  if (!(inputfile = debugfs_create_file("input", 00700, dir, NULL, &dbfs_fops_write))){
+    printk("Cannot create inputfile\n");
+    return -1;
+  }
+  if (!(ptreefile = debugfs_create_file("ptree", 00700, dir, NULL, &dbfs_fops_read))){
+    printk("Cannot create ptreefile\n");
+    return -1;
+  }
+  
+  printk("dbfs_ptree module initialize done\n");
 
-#if 0
-        dir = debugfs_create_dir("ptree", NULL);
-        
-        if (!dir) {
-                printk("Cannot create ptree dir\n");
-                return -1;
-        }
-
-        inputdir = debugfs_create_file("input", , , , );
-        ptreedir = debugfs_create_("ptree", , , ); // Find suitable debugfs API
-#endif
-	
-	printk("dbfs_ptree module initialize done\n");
-
-        return 0;
+  return 0;
 }
 
 static void __exit dbfs_module_exit(void)
 {
-        // Implement exit module code
-	
-	printk("dbfs_ptree module exit\n");
+  debugfs_remove_recursive(dir);
+  printk("dbfs_ptree module exit\n");
 }
 
 module_init(dbfs_module_init);
